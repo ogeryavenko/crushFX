6426fb948f6ed78f60d59588a47a42eef0f4584b
ogeryavenko <ogeryavenk@protonmail.com>
2022-04-07 18:35:36 +0300

    initialization window

--- src/main/java/com/example/crushfx/Main.java
+++ src/main/java/com/example/crushfx/Main.java
@@ -1,18 +1,24 @@
 package com.example.crushfx;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Circle circle = new Circle(50);
+        circle.setCenterX(100);
+        circle.setCenterY(100);
+
+        Pane pane = new Pane();
+        pane.getChildren().add(circle);
+
+        Scene scene = new Scene(pane, 400, 400);
         stage.setScene(scene);
         stage.show();
     }
